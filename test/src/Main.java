3d5bc4b6f30c25a39e6e32e40133369ea02a2b73
root <root@qq.com>
2022-07-09 11:17:09 +0800

    first

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
+    private static final int a = 11;
     public static void main(String[] args) {
+
         System.out.println("hello git11");
         System.out.println("hello git22");
         System.out.println("hello git33");

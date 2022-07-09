d90170636544118e48ca897a74fe01a5e8a42e14
root <root@qq.com>
2022-07-09 11:16:08 +0800

    second 常数

--- test/src/Main.java
+++ test/src/Main.java
@@ -1,4 +1,5 @@
 public class Main {
+    public static final int a = 10;
     public static void main(String[] args) {
         System.out.println("hello git11");
         System.out.println("hello git22");
